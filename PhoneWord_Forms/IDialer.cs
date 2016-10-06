e052a5e60adb862af9bc0c2189dbd321203ec380
chrisestesaa01 <chris.estes@willowtreeapps.com>
2016-10-06 10:06:21 -0400

    Repo created. This is a Hello world app for a Xamarin forms project. Tutorial is here: https://developer.xamarin.com/guides/xamarin-forms/getting-started/hello-xamarin-forms/quickstart/

---
+++ PhoneWord_Forms/IDialer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PhoneWord_Forms
+{
+    public interface IDialer
+    {
+        bool Dial(string number);
+    }
+}
---
+++ iOS/PhoneDialer.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using PhoneWord_Forms.iOS;
+using System;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+
+namespace PhoneWord_Forms.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
